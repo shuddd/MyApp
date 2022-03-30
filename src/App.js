07365f4a98ddc49bb9b2527c885a064f3d4839f3
shuddd <shuddd@gmail.com>
2022-03-30 15:49:43 +0530

    change

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor='#042743'
       showAlert("Dark mode has been enabled","success")
-      document.title="MyApp : Dark Mode"
+      // document.title="MyApp : Dark Mode"
       // setInterval(() => {
       //   document.title="MyApp is amazing!"
       // }, 2000);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,11 +14,11 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <a className="nav-link" href="#">{props.aboutText}</a>
-        </li>
+        </li> */}
       </ul>
     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
       <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
